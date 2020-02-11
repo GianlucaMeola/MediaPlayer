9158239ef038a946e04702d25192ead3d8982dfa
Gianluca Meola <Gianluca.Meola@tlcmarketing.com>
2020-02-11 10:18:46 +0000

    angular updates

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule,
-  MatListModule,
-  MatSliderModule,
-  MatIconModule,
-  MatToolbarModule,
-  MatCardModule
-} from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 const AGModules = [
   MatButtonModule,
