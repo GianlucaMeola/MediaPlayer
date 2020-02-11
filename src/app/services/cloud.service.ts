616af9894dfff4c1b55ff16ff4af32c41803b685
Gianluca Meola <Gianluca.Meola@tlcmarketing.com>
2020-02-11 11:55:30 +0000

    bug fix

--- src/app/services/cloud.service.ts
+++ src/app/services/cloud.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@angular/core';
 import {FileDetails} from '../interfaces/music-file';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-
-const endpoint: string = "https://musicfileapi.azurewebsites.net/api/MusicFile";
+import { SettingsProvider } from '../config/settings.provider';
 
 @Injectable()
 export class CloudService {
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private settingsProvider: SettingsProvider,){}
 
-  getFiles(): Observable<FileDetails[]>{
-    return this.http.get<FileDetails[]>(endpoint);
+  async getFiles(){
+    try
+    {
+    return await this.http.get<FileDetails[]>(this.settingsProvider.configuration.BASEURL);
+    }
+    catch(HttpErrorResponse)
+    {
+      console.log(HttpErrorResponse)
+    }
   }
 }
