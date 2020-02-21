eb2eb623dc87c9ec44fef54700d27eb4fc85b890
Gianluca Meola <Gianluca.Meola@tlcmarketing.com>
2020-02-21 10:39:02 +0000

    add delete error and confirm modal

--- src/app/pages/musiclist/musiclist.component.ts
+++ src/app/pages/musiclist/musiclist.component.ts
@@ -3,6 +3,7 @@ import { CloudService } from 'src/app/services/cloud.service';
 import { FileDetails } from 'src/app/interfaces/music-file';
 import { MatDialog } from '@angular/material/dialog';
 import { AlertConfirmComponent } from 'src/app/alert-confirm/alert-confirm.component';
+import { AlertDialogComponent } from 'src/app/alert-dialog/alert-dialog.component';
 
 @Component({
   selector: 'musiclist-edit',
@@ -10,49 +11,70 @@ import { AlertConfirmComponent } from 'src/app/alert-confirm/alert-confirm.compo
   styleUrls: ['./musiclist.component.scss']
 })
 export class MusicListComponent implements OnInit {
-
   public isLoading = false;
   public files: Array<FileDetails> = [];
 
   constructor(public cloudService: CloudService, private dialog: MatDialog) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.loadMusic();
   }
 
-   async loadMusic(){
-    try
-    {
+  async loadMusic() {
+    try {
       this.isLoading = true;
-      (await this.cloudService.getFiles()).subscribe( files => {
+      (await this.cloudService.getFiles()).subscribe(files => {
         this.files = files;
       });
     }
-    catch(error)
-    {
+    catch (error) {
       console.log("error", error);
     }
-    finally
-    {
+    finally {
       this.isLoading = false;
     }
-   }
+  }
 
-   async delete(fileName?: string){
-     let isConfirmed = await this.openConfirmDialog();
-     if(isConfirmed){
-       console.log("isConfirmed");
-     }
-   }
+  async delete(fileName?: string) {
+    let isConfirmed = await this.openConfirmDialog();
+    if (!isConfirmed) return;
+    fileName = "https://mediaplayerstorage.blob.core.windows.net/musicfiles/1637177934337406960_2f5b2e08-496f-479a-aa37-b94d6f4d958d.mp3"
+    this.openConfirmDialog
+    this.isLoading = true;
+    var formData: any = new FormData();
+    try {
+      formData.append('fileName', fileName);
+      let res = await this.cloudService.delete(formData);
+      this.openAlertDialog('Success!', res.toString());
+    } catch (e) {
+      let errors = e.error;
+      errors.forEach(error => {
+        this.openAlertDialog("Error", error.description)
+      });
+    } finally {
+      this.isLoading = false;
+    }
+  }
 
-   edit(){
+  edit() {
     console.log("edit");
-   }
+  }
 
-   openConfirmDialog(title?: string, message?: string, button?: string): boolean {
-     let res: boolean = false;
-    const dialogRef = this.dialog.open(AlertConfirmComponent,{
-      data:{
+  openAlertDialog(title: string, message?: string, button?: string, ) {
+    const dialogRef = this.dialog.open(AlertDialogComponent, {
+      data: {
+        title: title,
+        message: message,
+        buttonText: {
+          cancel: button
+        }
+      },
+    });
+  }
+
+  async openConfirmDialog(title?: string, message?: string, button?: string): Promise<boolean> {
+    const dialogRef = this.dialog.open(AlertConfirmComponent, {
+      data: {
         title: title,
         message: message,
         buttonText: {
@@ -61,9 +83,6 @@ export class MusicListComponent implements OnInit {
       },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      return result
-    });
-    return res;
+    return await dialogRef.afterClosed().toPromise();
   }
 }
